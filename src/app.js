ed66c74a2556e623220cb4ed7c2defab74be7f44
dmitrymitenkoff <d.mitenkoff@icloud.com>
2021-10-07 18:15:13 +1300

    Updated mobile viewport styles

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const navigation = document.querySelector('.navigation');
 const button = document.querySelector('.btn-toggle');
 const menu = document.querySelector('.menu-list');
+const lines = document.querySelectorAll('.line');
 
 // Hide menu on open
 menu.classList.add('hide');
@@ -12,31 +13,25 @@ button.setAttribute('aria-label', 'Open the menu');
 // Toggle menu open/close
 function toggleMenu(status) {
   if (status === 'false') {
-    console.log('status false is ' + status);
-
     menu.classList.remove('hide');
     button.setAttribute('aria-expanded', 'true');
     button.setAttribute('aria-label', 'Close the menu');
-    console.log(button.getAttribute('aria-expanded'));
+    lines.forEach(line => line.classList.toggle('active'));
   } else {
-    console.log('status true is ' + status);
     menu.classList.add('hide');
     button.setAttribute('aria-expanded', 'false');
     button.setAttribute('aria-label', 'Open the menu');
+    lines.forEach(line => line.classList.toggle('active'));
   }
 }
 
 button.addEventListener('click', event => {
-  const lines = document.querySelectorAll('.line');
-  lines.forEach(line => line.classList.toggle('active'));
   const status = button.getAttribute('aria-expanded');
   toggleMenu(status);
-  console.log(event.target);
 });
 
 // Close menu when user tabs outside the menu
 document.addEventListener('focusin', event => {
-  console.log(event.target);
   if (event.target.closest('nav') !== navigation) {
     toggleMenu(true);
   }
@@ -44,7 +39,6 @@ document.addEventListener('focusin', event => {
 
 // Close menu when user clicks outside the menu
 document.addEventListener('click', event => {
-  console.log(event.target);
   if (event.target.closest('nav') !== navigation) {
     toggleMenu(true);
   }

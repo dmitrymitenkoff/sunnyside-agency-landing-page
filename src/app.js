64a444fb8735a018107ceb3ea0639c54b602ca68
dmitrymitenkoff <d.mitenkoff@icloud.com>
2021-10-08 11:58:10 +1300

    Updated JS with a media query for the hamburger button and menu

--- src/app.js
+++ src/app.js
@@ -3,12 +3,22 @@ const button = document.querySelector('.btn-toggle');
 const menu = document.querySelector('.menu-list');
 const lines = document.querySelectorAll('.line');
 
-// Hide menu on open
-menu.classList.add('hide');
-
 // Set initial state for the button
 button.setAttribute('aria-expanded', 'false');
 button.setAttribute('aria-label', 'Open the menu');
+
+const mediaQuery = window.matchMedia('(max-width: 30em)');
+
+function handleMedia(e) {
+  if (e.matches) {
+    menu.classList.add('hide');
+  } else {
+    menu.classList.remove('hide');
+  }
+}
+
+mediaQuery.addEventListener('change', handleMedia);
+handleMedia(mediaQuery);
 
 // Toggle menu open/close
 function toggleMenu(status) {
@@ -31,15 +41,15 @@ button.addEventListener('click', event => {
 });
 
 // Close menu when user tabs outside the menu
-document.addEventListener('focusin', event => {
-  if (event.target.closest('nav') !== navigation) {
-    toggleMenu(true);
-  }
-});
+// document.addEventListener('focusin', event => {
+//   if (event.target.closest('nav') !== navigation) {
+//     toggleMenu(true);
+//   }
+// });
 
 // Close menu when user clicks outside the menu
-document.addEventListener('click', event => {
-  if (event.target.closest('nav') !== navigation) {
-    toggleMenu(true);
-  }
-});
+// document.addEventListener('click', event => {
+//   if (event.target.closest('nav') !== navigation) {
+//     toggleMenu(true);
+//   }
+// });
